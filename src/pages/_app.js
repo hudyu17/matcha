698e7cbac7b61daf6d5c7022bacd83b3b1eba723
hudyu17 <hengjeung.yuen@gmail.com>
2023-02-09 10:26:22 +0000

    plausible

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
 import '@/styles/globals.css'
+import PlausibleProvider from 'next-plausible'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <PlausibleProvider domain="careermatcha.com">
+      <Component {...pageProps} />
+    </PlausibleProvider>
+  )
 }
