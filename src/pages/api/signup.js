6584fec524f5facfd5f753bb181907d7e46e136f
hudyu17 <hengjeung.yuen@gmail.com>
2023-02-11 01:08:31 +0000

    ignore existing?

--- src/pages/api/signup.js
+++ src/pages/api/signup.js
@@ -7,32 +7,32 @@ export default function signup(req, res) {
 
     try {
         // Check if email exists
-        base('Emails')
-        .select({
-            filterByFormula: `Email = "${emailAdd}"`,
-            maxRecords: 1,
-        }).firstPage(function(err, records){
-            if (err) { 
-                console.error(err); 
-                return; 
-            }
-            if (records.length > 0) {
-                res.status(405).send('Email already exists!')
-                return;
-            } 
+        // base('Emails')
+        // .select({
+        //     filterByFormula: `Email = "${emailAdd}"`,
+        //     maxRecords: 1,
+        // }).firstPage(function(err, records){
+        //     if (err) { 
+        //         console.error(err); 
+        //         return; 
+        //     }
+        //     if (records.length > 0) {
+        //         res.status(405).send('Email already exists!')
+        //         return;
+        //     } 
         
         // Email does not exist, so add email to list
         base('Emails').create({
             "Email": emailAdd
         });
-        base('Emails')
-        .select({
-            filterByFormula: `Email = "${emailAdd}"`,
-            maxRecords: 1,
-        })
+        // base('Emails')
+        // .select({
+        //     filterByFormula: `Email = "${emailAdd}"`,
+        //     maxRecords: 1,
+        // })
         res.status(200).json({ message: 'Email added successfully' })
     
-        })
+        
         
     } catch (error) {
         res.status(400).send(error.message);
