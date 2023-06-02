81628df76d458a65765007ba101f7f7482ea853b
hudyu17 <hengjeung.yuen@gmail.com>
2023-06-02 15:10:53 +0800

    navbar testing

--- tests/navbar.spec.js
+++ tests/navbar.spec.js
@@ -1,10 +1,15 @@
 import { test, expect } from '@playwright/test';
 
+// skip if mobile
+test.skip(({ isMobile }) => isMobile);
+
 // Test navbar on every page
-test.afterEach(async ({ page }) => {
+test.afterEach(async ({ page, browserName }) => {
     // Runs after each test by checking navbar components
 
-    // will purposely fail on mobile
+    // skip if mobile
+    // test.skip(browserName != 'chromium', 'Mobile navbar different');
+
     await expect(page.getByRole('link', { name: 'Browse' })).toBeVisible();
     await expect(page.getByRole('link', { name: 'Insights' })).toBeVisible();
     await expect(page.getByRole('link', { name: 'Mentors' })).toBeVisible();
